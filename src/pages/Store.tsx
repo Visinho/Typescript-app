7ae6ab8e963b9897ae8b62e65eac7bd75840fd0b
Visinho <Elvisvisinho@rocketmail.com>
2023-09-26 02:03:30 +0100

    styling the Store Page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import storeItems from "../data/items.json";
 import {Col, Row} from "react-bootstrap";
+import StoreItem from '../components/StoreItem';
 
 const Store = () => {
   return (
@@ -10,7 +11,7 @@ const Store = () => {
     </h1>
     <Row md={2} xs={1} lg={3} className='g-3'>
       {storeItems.map(item => (
-        <Col>{JSON.stringify(item)}</Col>
+        <Col key={item.id}><StoreItem {...item} /></Col>
       ))}
     </Row>
   </>
