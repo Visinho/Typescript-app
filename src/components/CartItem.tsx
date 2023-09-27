8a9b5d4eaef150632e1a4a76173f79f7c8cf4177
Visinho <Elvisvisinho@rocketmail.com>
2023-09-27 12:15:55 +0100

    Set states in the local storage using typescript

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import storeItems from "../data/items.json";
 import { Stack, Button } from "react-bootstrap";
